099d6995a571d96abf07fd23916a49b116b50ce9
Konstantin Vorobev <riflex80@gmail.com>
2024-04-09 23:18:52 +0300

    Add project files.

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sampleapi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet(Name = "health")]
+        public IActionResult Get()
+        {
+            return new JsonResult(new { status = "OK" });
+        }
+    }
+}
+
